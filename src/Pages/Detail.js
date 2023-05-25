28f1b8582c6c938f35e257496896f1b081c92d9f
LAPTOP-MH6K4TCI\12mou <12mourad4gmail.com>
2023-05-25 15:02:08 +0200

    erreur chargement

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { useNavigate, useParams } from 'react-router-dom';
 
-function Recherche() {
+export default function Detail() {
     const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    const navigate = useNavigate()
     const { id } = useParams()
+    console.log(id)
 
 
     useEffect(() => {
@@ -17,9 +16,9 @@ function Recherche() {
 
     const fetchData = async () => {
         try {
-            const response = await axios.get(`https://api.spacexdata.com/v4/crew`)
-            setData(response.data)
+            const response = await axios.get(`https://api.spacexdata.com/v4/crew/${id}`)
             setIsLoading(false)
+            setData(response.data[0])
             console.log(response.data)
         } catch (error) {
             console.error(error)
@@ -29,15 +28,11 @@ function Recherche() {
     return (
         <>
             {isLoading ? <h1>loading </h1> : <div>
-                <h1>Liste Membres Crew :</h1>
+                <h1>Détail sur le membre :</h1>
                 <ul>
                     {data.map((item) => (
                         <Card key={item.name} style={{ width: '18rem' }}>
-                            <Card.Body
-                                onClick={() => {
-                                    navigate(`/detail/${item.id}`)
-                                }}
-                            >
+                            <Card.Body>
                                 <li style={{
                                     listStyle: 'none',
                                 }} >
@@ -50,19 +45,11 @@ function Recherche() {
                                 <Card.Text>
                                     Voici les informations sur le membre de l'équipage : {item.name}
                                 </Card.Text>
-                                <Button variant="primary">En savoir plus</Button>
                             </Card.Body>
                         </Card>
-
-
-
-
                     ))}
                 </ul>
             </div>}
-        </>)
-
-
+        </>
+    )
 }
-
-export default Recherche;
--- src/Router/Routeur.js
+++ src/Router/Routeur.js
@@ -4,6 +4,7 @@ import Layout from '../Components/Layout'
 import Home from '../Pages/Home'
 import NoMatch from '../Pages/NoMatch'
 import Liste from '../Pages/Liste'
+import Detail from '../Pages/Detail'
 
 const Routeur = () => {
     return (
@@ -12,7 +13,7 @@ const Routeur = () => {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/liste' element={<Liste />} />
-                    {/* <Route path='/detail/:id' element={< />} /> */}
+                    <Route path='/detail/:id' element={<Detail />} />
                     <Route path='*' element={<NoMatch />} />
                 </Routes>
             </Layout>
