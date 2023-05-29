3c63ecf57dfbe29a2a87d9e0df44023ca567b06a
Moranoo <92519044+Moranoo@users.noreply.github.com>
2023-05-29 18:31:44 +0200

    ajout menu burger format mobile

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,37 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Styles/components/Layout.css';
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <header>
-                <title>Space X</title>
-                <Nav justify variant="tabs" defaultActiveKey="/fuseur" className="navigation">
-                    <Nav.Item>
-                        <Nav.Link href="/crew">Liste des membres</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey href="/Fuseur">
-                            Liste des fusées
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/history">Histoire</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/company">À propos de SpaceX</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/quiz">Quiz</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </header>
-            <main>{children}</main>
-            <footer>&copy; 2023 IPSSI SQY</footer>
-        </>
-    );
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <>
+      <header>
+        <title>Space X</title>
+        {isMobile && (
+          <div className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+            <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`} />
+            <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`} />
+            <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`} />
+          </div>
+        )}
+        <Nav className={`navigation ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
+          <Nav.Item>
+            <Nav.Link href="/crew">Liste des membres</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey href="/Fuseur">
+              Liste des fusées
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/history">Histoire</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/company">À propos de SpaceX</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/quiz">Quiz</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </header>
+      <main>{children}</main>
+      <footer>&copy; 2023 IPSSI SQY</footer>
+    </>
+  );
 };
 
 export default Layout;
